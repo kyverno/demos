e6692302f843d5b4a31f206fe7be34227c262fc3
Ritesh Patel <ritesh@nirmata.com>
2023-03-01 16:18:04 -0800

    Fix error
    Signed-off-by: Ritesh Patel <ritesh@nirmata.com>

--- secure_self_service/argocd/install.sh
+++ secure_self_service/argocd/install.sh
@@ -4,7 +4,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 sleep 30
-kubectl -n argocd patch deployment argocd-server --type=json  \
-    -p='[{"op": "add", "path": "/spec/template/spec/securityContext/fsGroup", "value": 999}]'
-kubectl -n argocd patch statefulset argocd-application-controller --type=json \
-    -p='[{"op": "add", "path": "/spec/template/spec/securityContext/fsGroup", "value": 999}]'
+kubectl -n argocd patch deployment argocd-server --type=json -p='[{"op": "add", "path": "/spec/template/spec/securityContext/fsGroup", "value": 999}]'
+kubectl -n argocd patch statefulset argocd-application-controller --type=json -p='[{"op": "add", "path": "/spec/template/spec/securityContext/fsGroup", "value": 999}]'
